c9ec0d82f50e55214f62655c4fdb552ae860b0d6
hong <sjsjsj717@naver.com>
2020-08-06 18:08:56 +0900

    mainpage carousel design updating..

--- frontend/src/components/homepage/Carousel.vue
+++ frontend/src/components/homepage/Carousel.vue
@@ -7,6 +7,8 @@
       controls
       indicators
       background="#ababab"
+      img-width="1024"
+      img-height="480"
       style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
@@ -20,38 +22,16 @@
 
       <!-- Slides with custom text -->
       <b-carousel-slide img-src="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80">
-        <h1>당신만의 블로그를 <br>제작해보세요!</h1>
+        <h1 class="post">당신만의 블로그를 <br>제작해보세요!</h1>
       </b-carousel-slide>
 
       <!-- Slides with image only -->
       <b-carousel-slide img-src="https://images.unsplash.com/photo-1497005367839-6e852de72767?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=1047&q=80">
         
       <div class="signBtn">
-          <h2 class="ddd">회원가입</h2>
-        </div>
+          <h2>회원가입</h2>
+      </div>
       </b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <!-- <b-carousel-slide>
-        <template v-slot:img>
-          <imga
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide> -->
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <!-- <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide> -->
     </b-carousel>
   </div>
 </template>
@@ -76,9 +56,17 @@ export default {
   width: 50%;
   height: auto;
   border-radius: 20px;
+  position: relative;
+  left:400px;
+  bottom: 270px;
 }
-.ddd {
+.post {
+  position: relative;
+  left:400px;
+  bottom:270px;
+}
+/* .ddd {
   right: 20%;
   height:auto;
-}
+} */
 </style>
